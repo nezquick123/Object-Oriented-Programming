af66828ad471773b3e0ceb315fccc7cdd466db76
nezquick123 <57602308+nezquick123@users.noreply.github.com>
2022-11-06 21:29:08 +0100

    Update ex-1-2.cpp

--- lesson-1/ex-1-2.cpp
+++ lesson-1/ex-1-2.cpp
@@ -9,7 +9,7 @@ public:
     int number_of_PCs;
     int number_of_desks;
     string name;
-    void set_parameters(string name, int pc_count, int desk_count){
+    void parameters(string name, int pc_count, int desk_count){
         this->name = name;
         number_of_desks = desk_count;
         number_of_PCs = pc_count;
@@ -25,11 +25,11 @@ public:
   string color;
   string brand;
   string lab_name;
-  void set_parameters(string brand, string color){
+  void parameters(string brand, string color){
   this->brand = brand;
   this->color = color;
   }
-  void set_lab(Laboratory lab){
+  void lab(Laboratory lab){
   lab_name = lab.name;
   }
     void view(){
@@ -42,9 +42,9 @@ int main()
 {
     Keyboard keyb1;
     Laboratory lab1;
-    keyb1.set_parameters("natec", "black");
-    lab1.set_parameters("L.2.6.21", 16, 16);
-    keyb1.set_lab(lab1);
+    keyb1.parameters("natec", "black");
+    lab1.parameters("L.2.6.21", 16, 16);
+    keyb1.lab(lab1);
     keyb1.view();
     return 0;
 }
