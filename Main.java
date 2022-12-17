e36c49e06fe0fa6a70d10f4f79285f0303b6f348
nezquick123 <57602308+nezquick123@users.noreply.github.com>
2022-12-17 23:30:20 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import pl.poznan.put.Number;
+import pl.poznan.put.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Number format: <prefix> <value>");
+        System.out.println("Examples: 0x12 - hexadecimal, 012 - octal, b10 - binary, 12 - decimal");
+        System.out.println("Enter a number or expression to calculate: ");
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        Expression exp = new Expression(input);
+        Number outp = exp.calculatedVal();
+        Decimal decout = outp.Dec();
+        HexDec hex = outp.Hex();
+        Octal oct = outp.Oct();
+        Binary bin = outp.Bin();
+        System.out.println("Dec: " + decout.value());
+        System.out.println("Hex: " + hex.value());
+        System.out.println("Oct: " + oct.value());
+        System.out.println("Bin: " + bin.value());
+    }
+}
