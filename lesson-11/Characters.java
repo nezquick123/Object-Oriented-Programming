5c5103dd066070bcdd11fdf6002ff4f5540627d8
nezquick123 <57602308+nezquick123@users.noreply.github.com>
2023-01-19 16:31:13 +0100

    Create Characters.java

---
+++ lesson-11/Characters.java
@@ -0,0 +1,7 @@
+public class Characters implements Sequence{
+    private String characters;
+    public Characters(String ch){
+        characters = ch;
+    }
+    public String printedSeq(){return characters;}
+}
