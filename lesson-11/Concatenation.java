6b6ad88b7b95f8a9b5778e9fd6901fd2c501b9c1
nezquick123 <57602308+nezquick123@users.noreply.github.com>
2023-01-19 16:34:12 +0100

    Update Concatenation.java

--- lesson-11/Concatenation.java
+++ lesson-11/Concatenation.java
@@ -2,10 +2,10 @@ public class Concatenation implements Sequence {
         private final Sequence first;
         private final Sequence second;
         public String printedSeq() {
-            return this.first.printedSeq()+this.second.printedSeq();
+            return first.printedSeq()+this.second.printedSeq();
         }
         public Concatenation(Sequence fir, Sequence sec) {
-            this.first= fir;
-            this.second = sec;
+            first= fir;
+            second = sec;
         }
     }
